fc634036140f02e48cd729f9dcc13eea4c69a3fd
ggilabert <ggilabert@gmail.com>
2017-06-05 17:59:07 +0200

    creada plantilla list_view

--- news/views.py
+++ news/views.py
@@ -1,6 +1,13 @@
+from datetime import datetime
 from django.shortcuts import render_to_response
+from news.models import News
+
 
 # Create your views here.
 
 def news_list(request):
-    return render_to_response("news/news_list.html")
+    news = News.objects.filter(
+        publish_date__lte = datetime.now()).order_by('publish_date')
+
+    return render_to_response("news/news_list.html",
+                              {'news': news})
